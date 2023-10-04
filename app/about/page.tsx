7bbca1a6e84850d308fa03e9c4ebd4809849728e
Taylor Svec <taylorsvec978@yahoo.com>
2023-10-04 06:35:47 -0400

    fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,7 +18,7 @@ export default function About() {
 					alt="Profile"
 					className="w-48 rounded-full border-2 border-slate-800"
 				/>
-				<p className="indent-4 text-2xl text-app-black-2 md:w-3/4 lg:max-w-3xl">
+				<p className="text-2xl text-app-black-2 md:w-3/4 lg:max-w-3xl">
 					My name is{' '}
 					<span className="w-max bg-gradient-to-r from-app-blue to-app-green bg-clip-text pb-3 text-2xl font-bold text-transparent">
 						Taylor Svec
@@ -28,13 +28,13 @@ export default function About() {
 					working for. I had always been interested in getting into coding and this was a
 					great way to get started.
 				</p>
-				<p className="indent-4 text-2xl text-app-black-2 md:w-3/4 lg:max-w-3xl">
+				<p className="text-2xl text-app-black-2 md:w-3/4 lg:max-w-3xl">
 					Fast forward to today I am launching a small AI integration startup, working on
 					open source projects, looking for businesses to work with on updating and
 					creating web presences, and learning as many new technologies as I can with
 					personal projects.
 				</p>
-				<p className="indent-4 text-2xl text-app-black-2 md:w-3/4 lg:max-w-3xl">
+				<p className="text-2xl text-app-black-2 md:w-3/4 lg:max-w-3xl">
 					Please feel free to reach out with any opportunities!
 				</p>
 			</div>
