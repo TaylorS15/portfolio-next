b127292ef236ebb671cb74842c423858fd32b796
Taylor Svec <taylorsvec978@yahoo.com>
2023-10-04 06:46:30 -0400

    fixes

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -6,126 +6,107 @@ import Email from '../assets/email.svg';
 import Github from '../assets/github.svg';
 
 export default function ContactForm() {
-  const [formData, setFormData] = useState({
-    from_name: '',
-    from_email: '',
-    message: '',
-  });
+	const [formData, setFormData] = useState({
+		from_name: '',
+		from_email: '',
+		message: '',
+	});
 
-  const [formStatus, setFormStatus] = useState<'SUCCESS' | 'ERROR' | 'WAITING'>(
-    'WAITING',
-  );
+	const [formStatus, setFormStatus] = useState<'SUCCESS' | 'ERROR' | 'WAITING'>('WAITING');
 
-  function submitContactForm(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+	function submitContactForm(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
 
-    emailjs
-      .send(
-        'service_18kut6i',
-        'template_2591zf9',
-        formData,
-        'JRIASRf_UAV7BhpQz',
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setFormStatus('SUCCESS');
-          setFormData({ from_name: '', from_email: '', message: '' });
-          setTimeout(() => {
-            setFormStatus('WAITING');
-          }, 5000);
-        },
-        (error) => {
-          setFormStatus('ERROR');
-          setTimeout(() => {
-            setFormStatus('WAITING');
-          }, 5000);
-          console.log(error.text);
-        },
-      );
-  }
+		emailjs.send('service_18kut6i', 'template_2591zf9', formData, 'JRIASRf_UAV7BhpQz').then(
+			(result) => {
+				setFormStatus('SUCCESS');
+				setFormData({ from_name: '', from_email: '', message: '' });
+				setTimeout(() => {
+					setFormStatus('WAITING');
+				}, 5000);
+			},
+			(error) => {
+				setFormStatus('ERROR');
+				setTimeout(() => {
+					setFormStatus('WAITING');
+				}, 5000);
+				console.log(error.text);
+			}
+		);
+	}
 
-  return (
-    <div className="z-10 mx-[10vw] md:mx-[20vw] lg:mx-[23vw] xl:mx-[27vw]">
-      <h1 className="w-max bg-gradient-to-r from-app-blue to-app-green bg-clip-text pb-3 text-4xl font-bold text-transparent">
-        Get In Touch!
-      </h1>
-      <p className="text-xl text-app-black-2">
-        Whether it&apos;s a potential job opportunity or a funny meme, send me
-        an email!
-      </p>
-      <div className="my-4">
-        <div className="flex gap-4">
-          <Image src={Email} alt="Email" className="h-8 w-8" />
-          <p className="text-lg text-app-black-2">tsvec15@yahoo.com</p>
-        </div>
-        <div className="flex gap-4">
-          <Image src={Github} alt="Github" className="h-8 w-8" />
-          <a
-            href="https://www.github.com/TaylorS15"
-            className="text-lg text-app-black-2 hover:underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            https://www.github.com/TaylorS15
-          </a>
-        </div>
-      </div>
+	return (
+		<div className="z-10 mx-[10vw] md:mx-[20vw] lg:mx-[23vw] xl:mx-[27vw]">
+			<h1 className="w-max bg-gradient-to-r from-app-blue to-app-green bg-clip-text pb-3 text-4xl font-bold text-transparent">
+				Get In Touch!
+			</h1>
+			<p className="text-xl text-app-black-2">
+				Whether it&apos;s a potential job opportunity or a funny meme, send me an email!
+			</p>
+			<div className="my-4">
+				<div className="flex gap-4">
+					<Image src={Email} alt="Email" className="h-8 w-8" />
+					<p className="text-lg text-app-black-2">tsvec15@yahoo.com</p>
+				</div>
+				<div className="flex gap-4">
+					<Image src={Github} alt="Github" className="h-8 w-8" />
+					<a
+						href="https://www.github.com/TaylorS15"
+						className="text-lg text-app-black-2 hover:underline"
+						target="_blank"
+						rel="noopener noreferrer">
+						https://www.github.com/TaylorS15
+					</a>
+				</div>
+			</div>
 
-      <form className="flex flex-col gap-4" onSubmit={submitContactForm}>
-        <input
-          type="text"
-          placeholder="Name"
-          value={formData.from_name}
-          onChange={(e) =>
-            setFormData({ ...formData, from_name: e.target.value })
-          }
-          required
-          maxLength={100}
-          className="w-full max-w-sm rounded-md border-b-2 border-slate-600 bg-gray-800 p-2 text-app-white focus:border-2 focus:outline-none"
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={formData.from_email}
-          onChange={(e) =>
-            setFormData({ ...formData, from_email: e.target.value })
-          }
-          required
-          maxLength={100}
-          className="w-full max-w-sm rounded-md border-b-2 border-slate-600 bg-gray-800 p-2 text-app-white focus:border-2 focus:outline-none"
-        />
-        <textarea
-          placeholder="Message"
-          maxLength={500}
-          value={formData.message}
-          onChange={(e) =>
-            setFormData({ ...formData, message: e.target.value })
-          }
-          required
-          className="h-32 max-h-64 w-full max-w-sm rounded-md border-b-2 border-slate-600 bg-gray-800 p-2 text-app-white focus:border-2 focus:outline-none"
-        />
-        <div className="flex gap-4">
-          <button
-            type="submit"
-            className="h-10 w-32 rounded-md border-2 border-dark-green bg-gradient-to-r px-3 font-medium text-app-black-2 backdrop-blur-md transition-all hover:from-app-blue hover:to-app-green"
-          >
-            Send
-          </button>
+			<form className="flex flex-col gap-4" onSubmit={submitContactForm}>
+				<input
+					type="text"
+					placeholder="Name"
+					value={formData.from_name}
+					onChange={(e) => setFormData({ ...formData, from_name: e.target.value })}
+					required
+					maxLength={100}
+					className="w-full max-w-sm rounded-md border-b-2 border-slate-600 bg-gray-800 p-2 text-app-white focus:border-2 focus:outline-none"
+				/>
+				<input
+					type="email"
+					placeholder="Email"
+					value={formData.from_email}
+					onChange={(e) => setFormData({ ...formData, from_email: e.target.value })}
+					required
+					maxLength={100}
+					className="w-full max-w-sm rounded-md border-b-2 border-slate-600 bg-gray-800 p-2 text-app-white focus:border-2 focus:outline-none"
+				/>
+				<textarea
+					placeholder="Message"
+					maxLength={500}
+					value={formData.message}
+					onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+					required
+					className="h-32 max-h-64 w-full max-w-sm rounded-md border-b-2 border-slate-600 bg-gray-800 p-2 text-app-white focus:border-2 focus:outline-none"
+				/>
+				<div className="flex gap-4">
+					<button
+						type="submit"
+						className="h-10 w-32 rounded-md border-2 border-dark-green bg-gradient-to-r px-3 font-medium text-app-black-2 backdrop-blur-md transition-all hover:from-app-blue hover:to-app-green">
+						Send
+					</button>
 
-          {formStatus === 'SUCCESS' && (
-            <div className="h-10 w-32 border-2 border-dark-green bg-gradient-to-r from-green-500 to-green-900 px-3 font-medium text-app-black-2">
-              <p className="mt-1 text-center">Success!</p>
-            </div>
-          )}
+					{formStatus === 'SUCCESS' && (
+						<div className="h-10 w-32 rounded-md border-2 border-dark-green bg-gradient-to-r from-green-500 to-green-800 px-3 font-medium text-app-black-2">
+							<p className="mt-1 text-center">Success!</p>
+						</div>
+					)}
 
-          {formStatus === 'ERROR' && (
-            <div className="h-10 border-2 border-dark-green bg-gradient-to-r from-red-500 to-red-900 px-3 font-medium text-app-black-2">
-              <p className="mt-1 text-center">Ooops! There was an error.</p>
-            </div>
-          )}
-        </div>
-      </form>
-    </div>
-  );
+					{formStatus === 'ERROR' && (
+						<div className="h-10 rounded-md border-2 border-dark-green bg-gradient-to-r from-red-500 to-red-800 px-3 font-medium text-app-black-2">
+							<p className="mt-1 text-center">Oops! There was an error.</p>
+						</div>
+					)}
+				</div>
+			</form>
+		</div>
+	);
 }
